96ba2182b6bedc64e19cf79af427b067f4e086dc
sn1ch <sn1ch@list.ru>
2019-07-02 20:32:46 +0300

    add 'where' in 'getter' method, personal information replace moved special variable,

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import psycopg2
 from datetime import datetime, timedelta
 from pprint import pprint
 
+authorization = "dbname=hm_postgressql_db user=sn1ch password=1234"
+
 
 def create_db():  # создает таблицы
-    with psycopg2.connect("dbname=hm_postgressql_db user=sn1ch password=1234") as conn:
+    with psycopg2.connect(authorization) as conn:
         with conn.cursor() as cur:
             cur.execute("""create table student (
                 id serial primary key,
@@ -24,20 +26,19 @@ def create_db():  # создает таблицы
 
 
 def get_students(course_id):  # возвращает студентов определенного курса
-    with psycopg2.connect("dbname=hm_postgressql_db user=sn1ch password=1234") as conn:
+    with psycopg2.connect(authorization) as conn:
         with conn.cursor() as cur:
             cur.execute(
                 'select student.name, course_id from student_course '
                 'join student on student_course.student_id = student.id')
-            student_course_list = cur.fetchall()
-            for item in student_course_list:
-                if item[1] == int(course_id):
-                    print(item[0])
+            cur.execute('select student.name from student_course '
+                        'join student on student_course.student_id = student.id where course_id=(%s)', (course_id,))
+            return print(cur.fetchall())
 
 
 def add_students(course_id, students):  # создает студентов и
     # записывает их на курс
-    with psycopg2.connect("dbname=hm_postgressql_db user=sn1ch password=1234") as conn:
+    with psycopg2.connect(authorization) as conn:
         with conn.cursor() as cur:
             for student in students:
                 cur.execute('insert into student (name, gpa, birth) values (%s, %s, %s)',
@@ -52,18 +53,17 @@ def add_students(course_id, students):  # создает студентов и
 
 
 def add_student(**student):  # просто создает студента
-    with psycopg2.connect("dbname=hm_postgressql_db user=sn1ch password=1234") as conn:
+    with psycopg2.connect(authorization) as conn:
         with conn.cursor() as cur:
             cur.execute('insert into student (name, gpa, birth) values (%s, %s, %s)',
                         (list(student.values())[0], list(student.values())[1], list(student.values())[2]))
 
 
 def get_student(student_id):
-    with psycopg2.connect("dbname=hm_postgressql_db user=sn1ch password=1234") as conn:
+    with psycopg2.connect(authorization) as conn:
         with conn.cursor() as cur:
-            cur.execute('select * from student')
-            student_list = cur.fetchall()
-            return print(student_list[int(student_id)])
+            cur.execute('select * from student where id=(%s)', (student_id,))
+            return print(cur.fetchall())
 
 
 if __name__ == '__main__':
@@ -72,19 +72,20 @@ if __name__ == '__main__':
     long_ago2 = now - timedelta(days=365 * 10)
     long_ago3 = now - timedelta(days=365 * 5)
 
-    students = [{'name': 'Mr Black2',
-                 'gpa': 3.4,
-                 'birth': long_ago},
-                {'name': 'Mr Pink2',
-                 'gpa': 2,
-                 'birth': long_ago2},
-                {'name': 'Mr Blue2',
-                 'gpa': 4.7,
-                 'birth': long_ago2}
-                ]
+    students = [
+        {'name': 'Mr Black2',
+         'gpa': 3.4,
+         'birth': long_ago},
+        {'name': 'Mr Pink2',
+         'gpa': 2,
+         'birth': long_ago2},
+        {'name': 'Mr Blue2',
+         'gpa': 4.7,
+         'birth': long_ago2}
+    ]
 
     create_db()
     add_student(name='Jnon Pen', gpa=2, birth=long_ago3)
-    get_student(1)
+    get_student(22)
     get_students(1)
     add_students(1, students)
